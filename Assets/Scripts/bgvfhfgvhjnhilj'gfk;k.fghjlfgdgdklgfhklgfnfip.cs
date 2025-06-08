1a20279bd9d01367996a9c1c8fee188e47bc0180
V1rtual3 <kazumusick@gmail.com>
2025-06-08 14:04:16 -0500

    fixed

--- Assets/Scripts/bgvfhfgvhjnhilj'gfk;k.fghjlfgdgdklgfhklgfnfip.cs
+++ Assets/Scripts/bgvfhfgvhjnhilj'gfk;k.fghjlfgdgdklgfhklgfnfip.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bgvfhfgvhjnhilj'gfk;k.fghjlfgdgdklgfhklgfnfip : MonoBehaviour
+public class bgvfhfgvhjnhiljgfk : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
